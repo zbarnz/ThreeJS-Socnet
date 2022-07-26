fc653c0fb6404048a80d81e5aafedb960d6fe991
zbarnz <zbarnz@yahoo.com>
2022-07-26 01:36:06 -0600

    sphere with normalmap and displacementmap

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React, { useRef, useState } from "react";
 import { TextureLoader } from 'three/src/loaders/TextureLoader';
 
 function App() {
-    function Box(props) {
+    function Sphere(props) {
     // This reference gives us direct access to the THREE.Mesh object
         const ref = useRef();
         // Hold state for hovered and clicked events
@@ -14,18 +14,22 @@ function App() {
         useFrame((state, delta) => (ref.current.rotation.y += 0.01));
 
         const colorMap = useLoader(TextureLoader, 'socnet.png');
+        const normalMap = useLoader(TextureLoader, 'NormalMap.png');
+        const displacementMap = useLoader(TextureLoader, 'DisplacementMap.png');
         // Return the view, these are regular Threejs elements expressed in JSX
         return (
             <mesh
                 {...props}
                 ref={ref}
+                position={[0, 0, 0]}
                 scale={clicked ? 1.5 : 1}
                 onClick={(event) => click(!clicked)}
                 onPointerOver={(event) => hover(true)}
                 onPointerOut={(event) => hover(false)}
             >
                 <sphereGeometry attach="geometry" args={[1, 128]} />
-                <meshStandardMaterial map={colorMap} displacementScale={0.2} />
+                <meshStandardMaterial map={colorMap} normalMap={normalMap} displacementMap={displacementMap} displacementScale={0.1}/>
+
             </mesh>
         );
     }
@@ -35,7 +39,7 @@ function App() {
             <Canvas>
                 <ambientLight />
                 <pointLight position={[10, 10, 10]} />
-                <Box />
+                <Sphere />
             </Canvas>
         </div>
     );
